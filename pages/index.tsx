79a2870a3616e6ee58be6b0a74d5956a97c1791d
VIVO\asus <yundamin@naver.com>
2023-03-05 15:57:53 +0900

    ddd

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,13 @@ import Link from 'next/link'
 
 export default function IndexPage() {
   return (
+    <>
     <div>
       Hello World. <Link href="/about">About</Link>
     </div>
+    <div>
+      click directory day  <Link href="/day/index">About</Link>
+    </div>
+    </>
   )
 }
